a9b321d8c9212bb07d5f7f952d9e24d03d1f2cac
strelizia <strelizia@outlook.com>
2022-03-03 03:44:48 +0000

    implement min_products

--- week3/createContentTrainingData.py
+++ week3/createContentTrainingData.py
@@ -26,7 +26,6 @@ general.add_argument("--output", default="/workspace/datasets/fasttext/output.fa
 # Consuming all of the product data will take over an hour! But we still want to be able to obtain a representative sample.
 general.add_argument("--sample_rate", default=1.0, type=float, help="The rate at which to sample input (default is 1.0)")
 
-# IMPLEMENT: Setting min_products removes infrequent categories and makes the classifier's task easier.
 general.add_argument("--min_products", default=0, type=int, help="The minimum number of products per category (default is 0).")
 
 args = parser.parse_args()
@@ -34,32 +33,41 @@ output_file = args.output
 path = Path(output_file)
 output_dir = path.parent
 if os.path.isdir(output_dir) == False:
-        os.mkdir(output_dir)
+    os.mkdir(output_dir)
 
 if args.input:
     directory = args.input
-# IMPLEMENT:  Track the number of items in each category and only output if above the min
 min_products = args.min_products
 sample_rate = args.sample_rate
+all_result = {}
+
+for filename in os.listdir(directory):
+    if filename.endswith(".xml"):
+        print("Processing %s" % filename)
+        f = os.path.join(directory, filename)
+        tree = ET.parse(f)
+        root = tree.getroot()
+        for child in root:
+            if random.random() > sample_rate:
+                continue
+            # Check to make sure category name is valid
+            if (child.find('name') is not None and child.find('name').text is not None and
+                child.find('categoryPath') is not None and len(child.find('categoryPath')) > 0 and
+                child.find('categoryPath')[len(child.find('categoryPath')) - 1][0].text is not None):
+                # Choose last element in categoryPath as the leaf categoryId
+                cat = child.find('categoryPath')[len(child.find('categoryPath')) - 1][0].text
+                # Replace newline chars with spaces so fastText doesn't complain
+                name = child.find('name').text.replace('\n', ' ')
+                entry = "__label__%s %s\n" % (cat, transform_name(name))
+                if cat not in all_result.keys():
+                    all_result[cat] = []
+                all_result[cat].append(entry)
+
 
 print("Writing results to %s" % output_file)
 with open(output_file, 'w') as output:
-    for filename in os.listdir(directory):
-        if filename.endswith(".xml"):
-            print("Processing %s" % filename)
-            f = os.path.join(directory, filename)
-            tree = ET.parse(f)
-            root = tree.getroot()
-            for child in root:
-                if random.random() > sample_rate:
-                    continue
-                # Check to make sure category name is valid
-                if (child.find('name') is not None and child.find('name').text is not None and
-                    child.find('categoryPath') is not None and len(child.find('categoryPath')) > 0 and
-                    child.find('categoryPath')[len(child.find('categoryPath')) - 1][0].text is not None):
-                      # Choose last element in categoryPath as the leaf categoryId
-                      cat = child.find('categoryPath')[len(child.find('categoryPath')) - 1][0].text
-                      # Replace newline chars with spaces so fastText doesn't complain
-                      name = child.find('name').text.replace('\n', ' ')
-                      output.write("__label__%s %s\n" % (cat, transform_name(name)))
+    for _, v in all_result.items():
+        if len(v)>=min_products:
+            for x in v:
+                output.write(x)
 
