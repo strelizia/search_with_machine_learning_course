46a36db9a5d7b222485be334ff6bab860919c0bf
strelizia <strelizia@outlook.com>
2022-03-03 21:31:14 +0000

    update createReviewLabels

--- week3/createReviewLabels.py
+++ week3/createReviewLabels.py
@@ -1,11 +1,18 @@
 import os
 import argparse
 from pathlib import Path
+import re
+
+from nltk.stem import SnowballStemmer
+
+stemmer = SnowballStemmer("english")
 
 def transform_training_data(title, comment):
-    # IMPLEMENT
-    return title + ' ' + comment
-
+    text = title + ' ' + comment
+    text = re.sub('[^\w\s]', ' ', text.lower())
+    if len(text)>0:
+        text = ' '.join([stemmer.stem(x) for x in text.split()])
+    return text
 
 # Directory for review data
 directory = r'/workspace/datasets/product_data/reviews/'
@@ -13,6 +20,7 @@ parser = argparse.ArgumentParser(description='Process some integers.')
 general = parser.add_argument_group("general")
 general.add_argument("--input", default=directory,  help="The directory containing reviews")
 general.add_argument("--output", default="/workspace/datasets/fasttext/output.fasttext", help="the file to output to")
+general.add_argument("--bucket_rating", action="store_true", help="whether to bucketize ratings")
 
 args = parser.parse_args()
 output_file = args.output
@@ -24,6 +32,13 @@ if os.path.isdir(output_dir) == False:
 if args.input:
     directory = args.input
 
+def rating_category(r):
+    print(f'rating: {r}')
+    if float(r) < 3.0:
+        return 'negative'
+    elif float(r) > 3.0:
+        return 'positive'
+    return 'neutral'
 
 print("Writing results to %s" % output_file)
 with open(output_file, 'w') as output:
@@ -38,4 +53,6 @@ with open(output_file, 'w') as output:
                     elif '<comment>' in line:
                         comment = line[13:len(line) - 11]
                     elif '</review>'in line:
-                      output.write("__label__%s %s\n" % (rating, transform_training_data(title, comment)))
+                        if args.bucket_rating:
+                            rating = rating_category(rating)
+                        output.write("__label__%s %s\n" % (rating, transform_training_data(title, comment)))
